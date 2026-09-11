7a31587aab99df1a804b70815b01e496a4eb4677
GabrielSelvenca <gabriel.selvenca@icloud.com>
2026-09-11 09:18:26 -0300

    :bug: fix: arrumando classe para mostrar e ocultar nav

--- home/home.js
+++ home/home.js
@@ -1,4 +1,4 @@
 const nav = document.querySelector('.navegacao');
 const menu = document.querySelector('#menu');
 
-menu.addEventListener('click', () => { nav.classList.toggle('ocultar-nav'); });
+menu.addEventListener('click', () => { nav.classList.toggle('ativo'); });
